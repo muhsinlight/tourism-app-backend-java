fb6002551ffbc8842f84a2ef04cdd1a82e73f07f
Muhsin <138796335+muhsinisik@users.noreply.github.com>
2024-11-19 21:28:58 +0300

    feat:Controller-service
    update ve Delete işlemleri yaptım

--- src/main/java/com/teknofest/turizm/service/PlaceService.java
+++ src/main/java/com/teknofest/turizm/service/PlaceService.java
@@ -3,9 +3,10 @@ package com.teknofest.turizm.service;
 import com.teknofest.turizm.model.Place;
 
 public interface PlaceService {
-     Place savePlace(Place place);
-     Place updatePlace(Place place);
-     void  deletePlace(Place place);
+     Place savePlace(Place uplace);
      Place getPlaceById(Long id);
+     Place updatePlace(Long id,Place place);
+     void  deletePlace(Long id);
+
 
 }
