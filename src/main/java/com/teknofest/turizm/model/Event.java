c1157b7d117f801a3cadf0414963884cbcf360f7
muhsinlight <isik79.hotmai@gmail.com>
2025-01-26 22:43:29 +0300

    Place modulune user_has_visited eklendi ardından MockDataLoader güncellendi en son olarak da event modeli güncellenip event ile ilgili gerekli işlemler yapıldı

--- src/main/java/com/teknofest/turizm/model/Event.java
+++ src/main/java/com/teknofest/turizm/model/Event.java
@@ -1,5 +1,6 @@
 package com.teknofest.turizm.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.teknofest.turizm.model.audit.AuditAll;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -18,7 +19,6 @@ public class Event extends AuditAll<String> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
@@ -26,18 +26,20 @@ public class Event extends AuditAll<String> {
     private String title;
     @Column
     private String description;
-
+    @Column(name = "place_id")
+    @ManyToOne
+    private Place place;
     @ManyToOne
     @JoinColumn(name = "location_id")
     private Location location;
-
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @Column(name = "start_time")
     private LocalDateTime startTime;
-
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @Column(name = "end_time")
     private LocalDateTime endTime;
 
-    public Event(Integer id, User user, String title, String description, Location location, LocalDateTime startTime, LocalDateTime endTime) {
+    public Event(Integer id, User user, String title, String description, Location location, LocalDateTime startTime, LocalDateTime endTime,Place place) {
         this.id = id;
         this.user = user;
         this.title = title;
@@ -45,6 +47,7 @@ public class Event extends AuditAll<String> {
         this.location = location;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.place = place;
     }
 
     public Event() {
@@ -106,4 +109,11 @@ public class Event extends AuditAll<String> {
         this.endTime = endTime;
     }
 
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
 }
